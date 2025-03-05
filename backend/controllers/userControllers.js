2b638aec026f08ed4bc57ea12d441dc095a7c66b
Nation47 <hananiyusuph98@gmail.com>
2025-03-05 12:34:30 +0300

    api setting

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,9 +1,11 @@
+import asyncHandler from "express-async-handler";
+
 // auth user / set Token
 // route POST/api/users/auth
 // access public
 
-const userAuth = (req, res) => {
+const userAuth = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Auth User'})
-};
+});
 
 export { userAuth };
