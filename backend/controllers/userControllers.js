a7d09a2c563116e9b1599202b30841665c2255cd
Nation47 <hananiyusuph98@gmail.com>
2025-03-06 14:51:13 +0300

    more on auth

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -5,6 +5,20 @@ import generateToken from "../utils/generateTokens.js";
 // @description userAuth/set Token // route POST/api/users/auth // @access public
 
 const userAuth = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))) {
+        generateToken(res, user.id);
+        res.status(201).json({
+            id: user.id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password')
+    }
     res.status(200).json({message: 'Auth User'})
 });
 
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -26,7 +26,11 @@ userSchema.pre('save', async function(next) {
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt)
-})
+});
+
+userSchema.methods.matchPassword = async function (enteredPasswords) {
+    return await bcrypt.compare(enteredPasswords, this.password)
+}
 
 const User = mongoose.model('User', userSchema);
 
